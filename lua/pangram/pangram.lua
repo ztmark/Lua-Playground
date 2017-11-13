b5659133a14cba4be6dfc76aa0cbae2cc31ff96b
Mark <shuyumark@gmail.com>
2017-11-13 20:36:00 +0800

    update

--- lua/pangram/pangram.lua
+++ lua/pangram/pangram.lua
@@ -5,18 +5,15 @@ local a = string.byte('a')
 
 local function is_pangram(s) 
     local letters = {}
+    local count = 0
     for i = 1, #s do
         local idx = string.byte(string.lower(string.sub(s, i, i))) - a + 1
-        if 0 < idx and idx <= 26 then
+        if 0 < idx and idx <= 26 and letters[idx] == nil then
             letters[idx] = true
+            count = count + 1
         end
     end
-    for i = 1, 26 do
-        if letters[i] == nil then
-            return false
-        end
-    end
-    return true
+    return count == 26
 end
 
 
